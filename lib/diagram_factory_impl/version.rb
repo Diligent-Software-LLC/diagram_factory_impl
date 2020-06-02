ac75944001df55292ce491868a09208a81cf9009
Bradley J. Tannor <bradleytannor@gmail.com>
2020-06-02 12:20:52 -0700

    Prepare Deployment
    Testing dependency bug.

--- lib/diagram_factory_impl/version.rb
+++ lib/diagram_factory_impl/version.rb
@@ -1,7 +1,7 @@
 # Copyright (C) 2020 Diligent Software LLC. All rights reserved. Released
 # under the GNU General Public License, Version 3. Refer LICENSE.txt.
 
-require 'diagram_factory_int'
+#require 'diagram_factory_int'
 
 # DiagramFactory.
 # @class_description
@@ -12,6 +12,6 @@ require 'diagram_factory_int'
 #   The elements are hashes containing diagrams. The keys are children
 #   instances, and the values are Sets. The value Set elements are particular
 #   kind diagrams.
-class DiagramFactory < DiagramFactoryInt
+class DiagramFactory #< DiagramFactoryInt
   VERSION = '1.0.0'.freeze()
 end
